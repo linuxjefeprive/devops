d89784946409c534fdf2386a0cbaef47be120430
bent <bent@bentubuntu.com>
2021-12-08 23:29:31 +0100

    Yes

---
+++ rpmrepo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum upgrade -yy
+
+sudo yum install epel-release -yy
+exit 0 
+
+
+
+
