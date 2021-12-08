d21319126a84aaf36be09079cccac4b537600453
bent <bent@bentubuntu.com>
2021-12-09 00:24:34 +0100

    Beter

--- rpmrepo.sh
+++ rpmrepo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-
-sudo yum upgrade -yy
-
+sudo yum install -y amazon-linux-extras
+sudo amazon-linux-extras enable epel java-openjdk11
+yum clean metadata
 sudo yum install epel-release -yy
 exit 0 
 
